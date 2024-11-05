1f8a5efe89bd402b94a61f84a403d8bdb0600b8e
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-11-05 16:13:04 -0700

    Fixed virtuals on user and added pre middleware to deleteMany the user's thoughts by _id when user is deleted

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 // Import the necessary modules
 import { Schema, model, Document } from "mongoose";
+import Thought from "./Thought"; // Ensure Thought model is imported
 
 // Create Interface for User
 interface IUser extends Document {
@@ -10,7 +11,7 @@ interface IUser extends Document {
 }
 
 // Create User Schema
-const userSchema = new Schema(
+const userSchema = new Schema<IUser>(
   {
     username: {
       type: String,
@@ -39,16 +40,42 @@ const userSchema = new Schema(
   },
   {
     timestamps: true,
-    virtuals: {
-      friendCount: {
-        get() {
-          return this.friends.length;
-        },
-      },
+    toJSON: {
+      virtuals: true, // Allow virtual properties when data is requested
+    },
+    toObject: {
+      virtuals: true, // Allow virtual properties when data is requested
     },
   }
 );
 
-const User = model("User", userSchema);
+// Virtual method for friend count
+// Virtuals can be dynamically generated properties that are not stored in the database so this get method allows us to
+// retrieve the length of the user's friends array field on query
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Pre-hook to remove associated thoughts when a user is deleted
+// using .pre method allows us to run this function before the remove() method
+// We pass in next as a parameter to tell Mongoose to move on to the next middleware function when this function is done
+userSchema.pre("findOneAndDelete", async function (next) {
+  // Get the user ID from the query - this is a Mongoose method that allows us to access the query object and its properties
+  const userId = this.getQuery()["_id"];
+  try {
+    // Find the user by ID and ensure it exists before accessing thoughts
+    const user = await this.model.findById(userId);
+    // If the user exists and has thoughts, delete the thoughts with deleteMany() and pass in the _id values from the user's thoughts array
+    if (user && user.thoughts.length > 0) {
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+    }
+    // Move on to the next middleware function
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+const User = model<IUser>("User", userSchema);
 
 export default User;
